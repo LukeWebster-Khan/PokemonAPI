ad6e143342ce97cc0a7ee575ba9a7c4559c3a493
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-08-31 21:34:00 +0100

    changed value of state

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,21 @@ import axios from "axios";
 
 const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
 const Card = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(["bulbasaur", "charmander", "squirtle"]);
 
   useEffect(() => {
     axios.get(baseUrl).then((response) => {
-      setData(response.data.results[0]);
+      setData(response.data.results[1]);
     });
   }, []);
+  let pokemonData = data;
   let name = data.name;
   if (!data) return null;
 
   return (
     <>
       <div>
-        His name is {name}
+        His name is {data.name} and he is {data.weight} kg
         <h1>{console.log(data.url)}</h1>
       </div>
     </>

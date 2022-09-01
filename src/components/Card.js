5dcad49f016cba9e7dc2b7e1b0fd6f20c8c72289
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-09-01 23:30:03 +0100

    styled card and fetched Charizard

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,47 @@
 import React, { useState, useEffect } from "react";
+import "../styles/Card.css";
 import axios from "axios";
 
-const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+const baseUrl = "https://pokeapi.co/api/v2/pokemon/6";
 const Card = () => {
-  const [data, setData] = useState(["bulbasaur", "charmander", "squirtle"]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     axios.get(baseUrl).then((response) => {
-      setData(response.data.results[1]);
+      setData(response.data);
     });
   }, []);
   let pokemonData = data;
   let name = data.name;
   if (!data) return null;
-
+  console.log(data.url);
   return (
     <>
       <div>
-        His name is {data.name} and he is {data.weight} kg
-        <h1>{console.log(data.url)}</h1>
+        {/* His name is {data.name} and he is {data.weight} kg */}
+        <p>{console.log(data)}</p>
+        <div className="card__container">
+          <div className="card__top">
+            <div className="card__header-content">
+              <div className="card__header-left">
+                <p>basic Pokemon</p>
+                <h5>Charmander</h5>
+              </div>
+              <div className="card__header-right">
+                <p>50 HP</p>
+                <p>*icon*</p>
+              </div>
+            </div>
+            <div className="card__image-wrapper">
+              <img src={data.sprites.front_default} alt="pokemon" />
+            </div>
+          </div>
+          <div className="card__bottom">
+            <div className="card__metrics">
+              <p>Lizard Pokemon. Length 2' 0", Weight: 19 lbs</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
